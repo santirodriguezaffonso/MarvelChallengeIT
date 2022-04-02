3db28692750fd73d06f858f47bfa036ba1383090
Santiago Rodriguez Affonso <rodriguezsant@icloud.com>
2022-04-02 09:30:03 -0300

    Initial Commit

---
+++ MarvelChallengeIT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelChallengeIT
+//
+//  Created by Santiago Rodriguez Affonso on 02/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
